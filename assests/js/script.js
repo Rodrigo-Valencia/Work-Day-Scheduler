d6f9a6651df56f18d225b41e32578c2d3acd2411
Rodrigo <rodvalenciaa@gmail.com>
2022-03-04 19:03:36 -0700

    added moment lib

--- assests/js/script.js
+++ assests/js/script.js
@@ -2,7 +2,7 @@ moment(Date);
 $("#currentDay").text(moment().format('dddd MMMM Do YYYY, h:mm a'));
 
 var currentTime = moment();
-//currentTime = currentTime.startOf("hour");
+currentTime = currentTime.startOf("hour");
 
 var beforeTime = moment().startOf('day').add(9, "hours");
 
